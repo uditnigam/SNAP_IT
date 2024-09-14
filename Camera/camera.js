c70519483386b831dfa20038a0098f0bb643dac6
Udit Nigam <rajnigam.rock443@gmail.com>
2024-09-14 17:15:04 +0530

    Adding Blink functionality to the Camera and Video

--- Camera/camera.js
+++ Camera/camera.js
@@ -1,6 +1,6 @@
 const cameraPageBackBtn = document.querySelector(".camera-container-back-btn");
 const galleryOpenButton = document.querySelector(".gallery-open-button");
-const insideActionContainer = document.querySelectorAll(".action-container>*")
+const insideActionContainer = document.querySelectorAll(".action-container>*");
 const actionContainer = document.querySelector(".action-container");
 const stopPauseContainer = document.querySelector(".stop-pause-container");
 const pauseButton = document.querySelector(".pause-button");
@@ -22,226 +22,240 @@ let data = [];
 let videoTimer = 0;
 let interval;
 const constraints = {
-    audio: false,
-    video: true
+  audio: false,
+  video: true,
 };
 
 const videoState = {
-    recordState: false,
-    pauseState: false,
-    message: ''
+  recordState: false,
+  pauseState: false,
+  message: "",
 };
 
 //ON CLICK OF BACK BUTTON ON CAMERA PAGE, PAGE NAVIGATES TO THE HOMEPAGE
 cameraPageBackBtn.addEventListener("click", (e) => {
-    window.location.href = "/SNAP_IT/index.html";
+  window.location.href = "/SNAP_IT/index.html";
 });
 //ON CLICK OF GALLERY OPEN BUTTON ON CAMERA PAGE, PAGE NAVIGATES TO THE GALLERY
 galleryOpenButton.addEventListener("click", (e) => {
-    window.location.href = "/SNAP_IT/Gallery/gallery.html";
+  window.location.href = "/SNAP_IT/Gallery/gallery.html";
 });
 
 //Creating a functionality to promt for  permission and open the device's camera if it is available
-navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => {
-        // const audioTrack = stream.getAudioTracks();
-        video.srcObject = stream;
-        recorder = new MediaRecorder(stream);
-        recorder.ondataavailable = (event) => data.push(event.data);
-        recorder.addEventListener("stop", function () {
-            createDataUrl("video");
-        })
-    })
-    .catch((error) => {
-        console.log(error)
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((stream) => {
+    // const audioTrack = stream.getAudioTracks();
+    video.srcObject = stream;
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => data.push(event.data);
+    recorder.addEventListener("stop", function () {
+      createDataUrl("video");
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 cameraCaptureBtn.addEventListener("click", (e) => {
-    if (cameraTimer > 0) {
-        setTimeout(function () {
-            captureImage();
-            cameraTimer = 0;
-        }, (cameraTimer + 1) * 1000);
-        countdownTimer();
-    }
-    else {
-        captureImage();
-        console.log("0")
-    }
-    timerCont.innerHTML = '';
+  if (cameraTimer > 0) {
+    setTimeout(function () {
+      captureImage();
+      cameraTimer = 0;
+    }, (cameraTimer + 1) * 1000);
+    countdownTimer();
+  } else {
+    captureImage();
+    console.log("0");
+  }
+  timerCont.innerHTML = "";
 });
-//Function Countdown Timer 
+//Function Countdown Timer
 function countdownTimer() {
-    let timeleft = cameraTimer;
-    let downloadTimer = setInterval(function () {
-        if (timeleft <= 0) {
-            clearInterval(downloadTimer);
-            document.getElementById("countdown").style.display = "none";
-        } else {
-            document.getElementById("countdown").style.display = "flex";
-            document.getElementById("countdown").innerHTML = timeleft;
-        }
-        timeleft -= 1;
-    }, 1000);
+  let timeleft = cameraTimer;
+  let downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("countdown").style.display = "none";
+    } else {
+      document.getElementById("countdown").style.display = "flex";
+      document.getElementById("countdown").innerHTML = timeleft;
+    }
+    timeleft -= 1;
+  }, 1000);
 }
 function captureImage() {
-    const canvas = document.createElement("canvas");
-    canvas.height = video.videoHeight;
-    canvas.width = video.videoWidth;
-    const tool = canvas.getContext("2d");
-    insideActionContainer.forEach((ele) => {
-        ele.classList.remove("activeBtn");
-    })
-    cameraCaptureBtn.classList.add("activeBtn");
-    tool.filter = newFilter;
-    tool.scale(zoomLevel, zoomLevel);
-    let x = (canvas.width / zoomLevel - canvas.width) / 2;
-    let y = (canvas.width / zoomLevel - canvas.height) / 2;
-    tool.drawImage(video, x, y);
+  const canvas = document.createElement("canvas");
+  canvas.height = video.videoHeight;
+  canvas.width = video.videoWidth;
+  const tool = canvas.getContext("2d");
+  insideActionContainer.forEach((ele) => {
+    ele.classList.remove("activeBtn");
+  });
+  cameraCaptureBtn.classList.add("activeBtn");
+  video.classList.add("camera-blink");
+  setTimeout(() => {
+    video.classList.remove("camera-blink");
+  }, 200);
+  tool.filter = newFilter;
+  tool.scale(zoomLevel, zoomLevel);
+  let x = (canvas.width / zoomLevel - canvas.width) / 2;
+  let y = (canvas.width / zoomLevel - canvas.height) / 2;
+  tool.drawImage(video, x, y);
 
-    createDataUrl("camera", canvas);
+  createDataUrl("camera", canvas);
 }
 
 videoRecordBtn.addEventListener("click", (e) => {
-
-    if (cameraTimer == 3) {
-        setTimeout(function () {
-            videoRecording();
-            console.log("3")
-        }, 4000);
-        countdownTimer();
-    } else if (cameraTimer == 5) {
-        setTimeout(function () {
-            videoRecording();
-            console.log("5")
-        }, 6000);
-        countdownTimer();
-    } else if (cameraTimer == 10) {
-        setTimeout(function () {
-            videoRecording();
-            console.log("10")
-        }, 11000);
-        countdownTimer();
-    } else {
-        videoRecording();
-        console.log("0")
-    }
+  if (cameraTimer == 3) {
+    setTimeout(function () {
+      videoRecording();
+      console.log("3");
+    }, 4000);
+    countdownTimer();
+  } else if (cameraTimer == 5) {
+    setTimeout(function () {
+      videoRecording();
+      console.log("5");
+    }, 6000);
+    countdownTimer();
+  } else if (cameraTimer == 10) {
+    setTimeout(function () {
+      videoRecording();
+      console.log("10");
+    }, 11000);
+    countdownTimer();
+  } else {
+    videoRecording();
+    console.log("0");
+  }
 });
 
 function videoRecording() {
-    if (videoState.recordState === false) {
-        insideActionContainer.forEach((ele) => {
-            ele.classList.remove("activeBtn");
-        })
-        videoRecordBtn.classList.add("activeBtn");
-        initialTimer();
-        pauseButton.addEventListener("click", pauseRecording);
-        stopButton.addEventListener("click", stopRecording);
-    }
-    startRecording();
-};
+  if (videoState.recordState === false) {
+    insideActionContainer.forEach((ele) => {
+      ele.classList.remove("activeBtn");
+    });
+    videoRecordBtn.classList.add("activeBtn");
+    initialTimer();
+    pauseButton.addEventListener("click", pauseRecording);
+    stopButton.addEventListener("click", stopRecording);
+  }
+  startRecording();
+}
 
 function startRecording() {
-    console.log("start videoState: ", videoState)
-    if (videoState.recordState === true && videoState.pauseState === false) {
-        stopPauseContainer.style.display = "flex";
-        actionContainer.style.display = "none";
-        startTimer();
-        recorder.start();
+  console.log("start videoState: ", videoState);
+  if (videoState.recordState === true && videoState.pauseState === false) {
+    stopPauseContainer.style.display = "flex";
+    actionContainer.style.display = "none";
+    startTimer();
+    recorder.start();
 
-        videoState.recordState = false;
-        videoState.pauseState = true;
-        console.log("start videoState: ", videoState)
-    } else {
-
-        videoState.recordState = true;
-        videoState.pauseState = false;
-    }
-};
+    videoState.recordState = false;
+    videoState.pauseState = true;
+    console.log("start videoState: ", videoState);
+  } else {
+    videoState.recordState = true;
+    videoState.pauseState = false;
+  }
+}
 
 function pauseRecording() {
-    clearInterval(interval);
-    // videoState.recordState = true;
-    // console.log("pause videoState: ", videoState)
-    if (videoState.pauseState === false) {
-        const resumedMsgBox = document.createElement("div");
-        resumedMsgBox.setAttribute("class", "resumed-message-box");
-        resumedMsgBox.innerHTML = `
+  clearInterval(interval);
+  // videoState.recordState = true;
+  // console.log("pause videoState: ", videoState)
+  if (videoState.pauseState === false) {
+    const resumedMsgBox = document.createElement("div");
+    resumedMsgBox.setAttribute("class", "resumed-message-box");
+    resumedMsgBox.innerHTML = `
             <div class="timer">Resumed</div>
       `;
-        messageCont.appendChild(resumedMsgBox);
-        setTimeout(function () { messageCont.removeChild(resumedMsgBox); }, 2000);
-        // videoState.pauseState = true;
-        // videoState.pauseState = true;
-    } else {
-        const pausedMsgBox = document.createElement("div");
-        pausedMsgBox.setAttribute("class", "paused-message-box");
-        pausedMsgBox.innerHTML = `
+    messageCont.appendChild(resumedMsgBox);
+    setTimeout(function () {
+      messageCont.removeChild(resumedMsgBox);
+    }, 2000);
+    // videoState.pauseState = true;
+    // videoState.pauseState = true;
+  } else {
+    const pausedMsgBox = document.createElement("div");
+    pausedMsgBox.setAttribute("class", "paused-message-box");
+    pausedMsgBox.innerHTML = `
                 <div class="timer">Paused</div>
           `;
-        messageCont.appendChild(pausedMsgBox);
-        setTimeout(function () { messageCont.removeChild(pausedMsgBox); }, 2000);
-        // videoState.pauseState = false;
-    }
-    startRecording();
-};
+    messageCont.appendChild(pausedMsgBox);
+    setTimeout(function () {
+      messageCont.removeChild(pausedMsgBox);
+    }, 2000);
+    // videoState.pauseState = false;
+  }
+  startRecording();
+}
 
 function stopRecording() {
-    stopPauseContainer.style.display = "none";
-    actionContainer.style.display = "flex";
-    clearTimeout(interval);
-    recorder.stop();
-    timerCont.innerHTML = "";
-    timerTime = 0;
-};
+  stopPauseContainer.style.display = "none";
+  actionContainer.style.display = "flex";
+  clearTimeout(interval);
+  recorder.stop();
+  timerCont.innerHTML = "";
+  timerTime = 0;
+  cameraCaptureBtn.classList.add("activeBtn");
+  video.classList.add("camera-blink");
+  setTimeout(() => {
+    video.classList.remove("camera-blink");
+  }, 200);
+}
 
 function startTimer() {
-    interval = setInterval(countUpTimer, 1000);
-    function countUpTimer() {
-        videoTimer++;
-        const numberMinutes = Math.floor(videoTimer / 60);
-        const numberSeconds = videoTimer % 60;
-        const timerContainer = document.createElement("div");
-        timerContainer.setAttribute("class", "timer-box");
-        timerContainer.innerHTML = `
+  interval = setInterval(countUpTimer, 1000);
+  function countUpTimer() {
+    videoTimer++;
+    const numberMinutes = Math.floor(videoTimer / 60);
+    const numberSeconds = videoTimer % 60;
+    const timerContainer = document.createElement("div");
+    timerContainer.setAttribute("class", "timer-box");
+    timerContainer.innerHTML = `
             <div class="red_circle"></div>
-            <div class="timer">${pad(numberMinutes)} : ${pad(numberSeconds)}</div>
+            <div class="timer">${pad(numberMinutes)} : ${pad(
+      numberSeconds
+    )}</div>
       `;
-        timerCont.appendChild(timerContainer);
-    }
-    const pad = (number) => {
-        return (number < 10) ? '0' + number : number;
-    }
-};
+    timerCont.appendChild(timerContainer);
+  }
+  const pad = (number) => {
+    return number < 10 ? "0" + number : number;
+  };
+}
 
 function initialTimer() {
-    const initialTimerContainer = document.createElement("div");
-    initialTimerContainer.setAttribute("class", "initial-timer-box");
-    initialTimerContainer.innerHTML = `
+  const initialTimerContainer = document.createElement("div");
+  initialTimerContainer.setAttribute("class", "initial-timer-box");
+  initialTimerContainer.innerHTML = `
             <div class="red_circle"></div>
             <div class="timer">00:00</div>
       `;
-    timerCont.appendChild(initialTimerContainer);
-};
+  timerCont.appendChild(initialTimerContainer);
+}
 
 //Function to create image and video url
 function createDataUrl(type, canvas) {
-    const date = currentDate();
-    if (type === "camera") {
-        const name = "Image_" + Date.now();
-        const link = canvas.toDataURL();
-        addData(link, "img", name, date);
-    } else {
-        const name = "Video_" + Date.now();
-        let blob = new Blob(data, { type: 'video/mp4' });
-        addData(blob, "video", name, date);
-        data = [];
-    }
-};
+  const date = currentDate();
+  if (type === "camera") {
+    const name = "Image_" + Date.now();
+    const link = canvas.toDataURL();
+    addData(link, "img", name, date);
+  } else {
+    const name = "Video_" + Date.now();
+    let blob = new Blob(data, { type: "video/mp4" });
+    addData(blob, "video", name, date);
+    data = [];
+  }
+}
 
 //Function to Get the current Date
 function currentDate() {
-    const date = new Date();
-    const str = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    return str;
-};
+  const date = new Date();
+  const str =
+    date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+  return str;
+}
--- homepage.js
+++ homepage.js
@@ -1,59 +1,62 @@
 const cameraBtn = document.querySelector(".right-container-camera-btn");
-const screenRecordingBtn = document.querySelector(".right-container-screen-recording-btn");
+const screenRecordingBtn = document.querySelector(
+  ".right-container-screen-recording-btn"
+);
 const galleryBtn = document.querySelector(".right-container-gallery-btn");
 
-
-
-
 cameraBtn.addEventListener("click", (e) => {
-    window.location.href = "/SNAP_IT/Camera/camera.html";
-})
+  window.location.href = "/SNAP_IT/Camera/camera.html";
+});
 
 galleryBtn.addEventListener("click", (e) => {
-    window.location.href = "/SNAP_IT/Gallery/gallery.html";
-})
+  window.location.href = "/SNAP_IT/Gallery/gallery.html";
+});
 
 const displayMediaOptions = {
-    video: {
-        displaySurface: "screen"
-    }
+  video: {
+    displaySurface: "screen",
+  },
 };
 let data = [];
 let recordState = false;
 let mediaRecorder;
 screenRecordingBtn.addEventListener("click", async function (e) {
-    await navigator.mediaDevices.getDisplayMedia(displayMediaOptions).then(function (stream) {
-        mediaRecorder = new MediaRecorder(stream);
-        mediaRecorder.addEventListener("dataavailable", (e) => {
-            data.push(e.data);
-        })
-        mediaRecorder.addEventListener("stop", (e) => {
-            createDataUrl();
-        })
-    }).catch(function (err) {
-        console.log(err)
+  await navigator.mediaDevices
+    .getDisplayMedia(displayMediaOptions)
+    .then(function (stream) {
+      mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.addEventListener("dataavailable", (e) => {
+        data.push(e.data);
+      });
+      mediaRecorder.addEventListener("stop", (e) => {
+        createDataUrl();
+      });
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 
-    if (recordState == false) {
-        mediaRecorder.start();
-        recordState = true;
-    } else {
-        mediaRecorder.stop();
-        recordState = false;
-    }
+  if (recordState == false) {
+    mediaRecorder.start();
+    recordState = true;
+  } else {
+    mediaRecorder.stop();
+    recordState = false;
+  }
 });
 
 function createDataUrl() {
-    const date = currentDate();
-    const name = "Screen_" + Date.now();
-    let blob = new Blob(data, { type: 'screen/mp4' });
-    addData(blob, "screen", name, date);
-    data = [];
-};
+  const date = currentDate();
+  const name = "Screen_" + Date.now();
+  let blob = new Blob(data, { type: "screen/mp4" });
+  addData(blob, "screen", name, date);
+  data = [];
+}
 
 //Function to Get Date
 function currentDate() {
-    const date = new Date();
-    const str = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-    return str;
-};
+  const date = new Date();
+  const str =
+    date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+  return str;
+}
